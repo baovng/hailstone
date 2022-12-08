3c6e61db9a1392dd979b7fa210051e747e308e26
dacinve <78516378+dacinve@users.noreply.github.com>
2022-12-08 00:45:30 -0500

    test

--- client.c
+++ client.c
@@ -28,43 +28,44 @@ int main(int argc, char **argv)
     connect_idx = 2; 
     
     Rio_readinitb(&rio, clientfd);
-    printf("-->Enter A Positive Integer: ");
-    scanf("%s", buf);
+    // printf("-->Enter A Positive Integer: ");
+    // scanf("%s", buf);
     
-    while ((strcmp(buff,"")0)) {
-    // while(1) {
-    //     printf("-->Enter A Positive Integer (^d to quit): ");
-    //     scanf("%s", myStr);
-    //     if(strcmp(myStr, "^d") == 0){
-    //             printf("--> Disconnected from localhost port x. Goodbye.\n");
-    //             Close(clientfd);
-    //             exit(0);
-    //     }if((strcmp(myStr, "0") == 0) || (strstr(myStr, "-") != NULL)||(strcmp(myStr, "") == 0)){
-    //         printf("Retry with possitive integer \n");
+    // while ((strcmp(buff,""))) {
+    while(1) {
+        printf("-->Enter A Positive Integer (^d to quit): ");
+        scanf("%s", buf);
+        if(strcmp(buf, "^d") == 0){
+                printf("--> Disconnected from localhost port x. Goodbye.\n");
+                Close(clientfd);
+                exit(0);
+        }if((strcmp(buf, "0") == 0) || (strstr(buf, "-") != NULL)||(strcmp(buf, "") == 0)){
+            printf("Retry with possitive integer \n");
             
-    //     }else{
-    //             //hailstone program here
-    //             // printf("Your String is %s\n",myStr);
-    //             // printf("count %d\n",connect_idx);
-    //             length = 1;
-    //             num = atoi(myStr);
-    //             printf("[HSS:%d:x] Sequence start with %d \n",connect_idx,num);
-    //             while(num != 1)
-    //             {
-    //                 if( num % 2 )  /* true if odd */
-    //                     num = (num*3)+1;
-    //                 else                /* even */
-    //                     num /= 2;
-    //                 printf("[HSS:%d:x] %d \n",connect_idx,num);
-    //                 length++;
-    //             }
-    //             printf("[HSS:%d:x] Finished.  Number of iterations = %d\n\n",connect_idx,(length-1));
-    //             connect_idx++;          
-    //         }
+        }else{
+                //hailstone program here
+                // printf("Your String is %s\n",myStr);
+                // printf("count %d\n",connect_idx);
+                Rio_writen(clientfd, buf, strlen(buf));
+                Rio_readlineb(&rio, buf, MAXLINE);
+                length = 1;
+                num = atoi(buf);
+                printf("[HSS:%d:x] Sequence start with %d \n",connect_idx,num);
+                while(num != 1)
+                {
+                    if( num % 2 )  /* true if odd */
+                        num = (num*3)+1;
+                    else                /* even */
+                        num /= 2;
+                    printf("[HSS:%d:x] %d \n",connect_idx,num);
+                    length++;
+                }
+                printf("[HSS:%d:x] Finished.  Number of iterations = %d\n\n",connect_idx,(length-1));
+                connect_idx++;          
+            }
         
     // }
-    printf(" F here %s", buf);
-    Rio_writen(clientfd, buf, strlen(buf));
+    // Rio_writen(clientfd, buf, strlen(buf));
     // Rio_readlineb(&rio, buf, MAXLINE);
     // Fputs(buf, stdout);
     
