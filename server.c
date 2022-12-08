3061e310035ede09e4da60cd8b6cb1442d8164e0
dacinve <78516378+dacinve@users.noreply.github.com>
2022-12-08 01:53:33 -0500

    test

--- server.c
+++ server.c
@@ -32,7 +32,7 @@ int main(int argc, char **argv)
 
 	int counter;
 	for(counter=0; counter<argc; counter++)
-		printf("argv[%2d]: %s\n",counter,argv[counter]);
+		printf("port %d, and %s",argv[1],argv[1]);
 
     while (1) {
 	clientlen = sizeof(struct sockaddr_storage); 
