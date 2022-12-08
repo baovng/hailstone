00b528cb6af65bbb0803c8c45deebe4c9ac55836
dacinve <78516378+dacinve@users.noreply.github.com>
2022-12-08 00:20:19 -0500

    test

--- compute.c
+++ compute.c
@@ -12,8 +12,7 @@ void compute(int connfd)
 
     Rio_readinitb(&rio, connfd);
     while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
-	printf("Received %d bytes\n", Rio_readlineb(&rio, buf, MAXLINE));
-    printf("Received %d bytes\n", Rio_writen(connfd, buf, n));
+	printf("Received %d bytes\n", n);
     printf("Received %s bytes\n", buf);
 	Rio_writen(connfd, buf, n);
     }
